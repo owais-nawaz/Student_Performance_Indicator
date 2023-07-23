de7e512eae36cc382632195a78b3e8cb567e7556
Syed Owais Nawaz <owaisnawaz17@gmail.com>
2023-07-23 19:56:29 +0300

    data ingestion

--- src/exception.py
+++ src/exception.py
@@ -1,5 +1,5 @@
 import sys, os
-import logging
+from src.logger import logging
 
 
 def error_message_detail(error, error_detail: sys):
@@ -20,3 +20,11 @@ class CustomException(Exception):
 
     def __str__(self):
         return self.error_message
+
+
+if __name__ == "__main__":
+    try:
+        a = 1 / 0
+    except Exception as e:
+        logging.info("divide by zero")
+        raise CustomException(e, sys)
